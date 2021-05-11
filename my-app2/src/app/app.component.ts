af54017159078087f8776d0b55602e5648ea9aac
Scott Walter <scott.walter@nice.com>
2021-05-10 20:44:37 -0500

    first commit

---
+++ my-app2/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app2-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'my-app2';
+  appVersion = VERSION;
+}
